89a95d43ee955ba5d7de079d56ee754b7f0f651f
shubham vora <shubhamvora05@gmail.com>
2021-03-28 08:57:48 +0530

    navbar added and some error resolved in login page

---
+++ appBRS/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import userInfo, User, Bike, Rent, Station, Employee, Payment
+
+admin.site.register(userInfo)
+admin.site.register(Bike)
+admin.site.register(Rent)
+admin.site.register(Station)
+admin.site.register(Employee)
+admin.site.register(Payment)
