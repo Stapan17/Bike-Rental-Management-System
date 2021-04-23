2dc656ae17dbb8a21410fdb6c7c3c20318c12131
Mohit Panchasara <mohitpanchasara@gmail.com>
2021-04-23 06:55:52 +0530

    bugs fixed

--- appBRS/urls.py
+++ appBRS/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.home, name="home"),
     path('register/', views.register_user, name='register_user'),
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
